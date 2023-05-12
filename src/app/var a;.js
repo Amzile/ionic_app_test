e190fb24e745ebbc22e1fa7d47b5582db240833a
Amzile Ntleki <NtlekiA@sabc.co.za>
2023-05-12 15:36:06 +0200

    first commit

---
+++ src/app/var a;.js
@@ -0,0 +1,9 @@
+var a;
+var b ;
+var myname = "boe"
+a = 2
+b = 4
+c = a + b
+console.log(myname)
+console.log(b, a)
+console.log(c)
---
+++ src/app/var arrayy = [12,24,12,58].js
@@ -0,0 +1,26 @@
+/*function oukmark(){
+    console.log("Never exposed to this");
+}
+
+function four4(){
+    console.log("Lets try this")
+}
+
+function too2you(){
+    console.log("If it never worked dont judge me")
+}
+
+too2you();
+oukmark();
+four4();*/
+
+/*function agiann(arrayy, nums){
+    arrayy.push(nums)
+    return arrayy.shift();  
+}
+var testArr = [1,2,3,4,5]
+console.log("before: " + JSON.stringify(testArr));
+console.log(agiann(testArr,6));
+console.log("After: " + JSON.stringify(testArr));*/
+
+
